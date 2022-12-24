2e2c16300ee5387c90c0061b4c0c3f9cad4399cf
valera-bagancha <valerabagancha@gmail.com>
2022-12-24 17:53:26 +0200

    clear proj

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './styles/App.css'
+import './styles/reset.css'
 
-function App() {
+const App = () => {
   return (
-    <div>Hello</div>
-  );
+   <></>
+  )
 }
 
-export default App;
+export default App
